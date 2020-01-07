213792dbad0579fc8a9cbb6a47231ec536033d54
letrungtiennbk9 <letrungtiennbk9@gmail.com>
2020-01-07 15:44:39 +0700

    Tien up cho admin page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const adminTask = require('../controllers/adminTask');
+const apiUrl = 'http://localhost:8080/';
+const request = require('request');
 
 /* GET home page. */
 router.get('/',adminTask.isLoggedIn, function(req, res, next) {
-  res.render('index',{title:'Top bán chạy'});
+  request(apiUrl + 'topTen', { json: true }, (err, rspnd, body) => {
+    for(let i=0; i<body.length; i++){
+      body[i].revenue = body[i].price * body[i].sold;
+    }
+    res.render('index', { title: "Top bán chạy", topTen: body });
+  });
 });
 
 module.exports = router;
